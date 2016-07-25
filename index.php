fd287439712a4dc484e37f2e1854fe8a23cdeda3
Alex Anderson <cryogenica0@gmail.com>
2016-07-24 19:26:08 -0500

    Changed index file (the website is under development)

---
+++ index.php
@@ -0,0 +1,112 @@
+<html>
+<head>
+	<title>Fancy CSS Demo</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Arvo:400" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<style type="text/css">
+.blank{}
+.page_title{
+	text-align: center;
+	font-size: 24px;
+}
+.header_bar{
+	text-align: center;
+}
+.top_bar{
+	color: #ccc;
+	font-family: 'Montserrat', sans-serif;
+	border-bottom-style: groove;
+	border-left-style: groove;
+	border-right-style: groove;
+	border-color: #333;
+}
+footer{
+	color: #777;
+	border-top-color: #333;
+	border-top-style: groove;
+	padding-top: 5pt;
+	text-align: center;
+	font-family: 'Arvo', sans-serif;
+}
+body{
+	background-color: #111;
+}
+a{
+	color: #777;
+	text-decoration: none;
+	font-family: 'Arvo', monospace;
+}
+a.sidebar_hrefs{
+	font-size: 13pt;
+	color: #777;
+}
+.body{
+	font-family: 'Arvo', sans-serif;
+	color: #888;
+	position: relative;
+	left: 10px;
+	font-size: 13pt;
+	width: 95%;
+	align: center;
+}
+p{
+	text-indent: 10px;
+}
+h3{
+	text-align: center;
+}
+hr{
+	border-color: #333;
+}
+	</style>
+</head>
+<body>
+	<div class="top_bar">
+		<div class="header_bar">
+			<span class="page_title">
+				&nbsp;CSS Template&nbsp;
+			</span>
+		</div>
+		<hr style="width: 15%;">
+		<br>
+		<table style="text-align: center; width: 100%">
+			<tr>
+				<td>
+					<a href="/link1.php" class="sidebar_href">Menu item 1</a>
+				</td>
+				<td>
+					<a href="/link2.php" class="sidebar_href">Menu item 2</a>
+				</td>
+				<td>
+					<a href="/link3.php" class="sidebar_href">Menu item 3</a>
+				</td>
+			</tr>
+		</table>
+		</div>
+	</div>
+	<div class="body">
+		<br>
+		<h3>
+			Article Title Here
+		</h3>
+		<p>
+A <a href="https://en.wikipedia.org/wiki/Paragraph">paragraph</a> (from the Ancient Greek παράγραφος paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.[1][2] Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.
+		</p>
+		<p>
+The oldest classical Greek and Latin writing had little or no space between words, and could be written in boustrophedon (alternating directions). Over time, text direction (left to right) became standardized, and word dividers and terminal punctuation became common. The first way to divide sentences into groups was the original paragraphos, similar to an underscore at the beginning of the new group.[3] The Greek paragraphos evolved into the pilcrow (¶), which in English manuscripts in the Middle Ages can be seen inserted inline between sentences. The hedera leaf (e.g. ☙) has also been used in the same way.
+		</p>
+		<p>
+Indented paragraphs demonstrated in the US Constitution
+In ancient manuscripts, another means to divide sentences into paragraphs was a line break (newline) followed by an initial at the beginning of the next paragraph. An initial is an oversize capital letter, sometimes outdented beyond the margin of text. This style can be seen, for example, in the original Old English manuscript of Beowulf. Outdenting is still used in English typography, though not commonly.[4] Modern English typography usually indicates a new paragraph by indenting the first line. This style can be seen in the (handwritten) United States Constitution from 1787. For additional ornamentation, a hedera leaf or other symbol can be added to the inter-paragraph whitespace, or put in the indentation space.
+		</p>
+		<p>
+A second common modern English style is to use no indenting, but add vertical whitespace to create "block paragraphs." On a typewriter, a double carriage return produces a blank line for this purpose; professional typesetters (or word processing software) may put in an arbitrary vertical space by adjusting leading. This style is very common in electronic formats, such as on the World Wide Web and email.
+		</p>
+	</div>
+	<footer>
+		Designed by Alex Anderson (2016). All rights reserved.
+	</footer>
+</body>
+</html>
